265581e1ad51e3b4e9f024ccd72853ba42a0e877
Erin Sullivan <kittylily@gmail.com>
2016-10-26 14:14:50 -0700

    📶 Connect to wifi

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 require("Font8x12").add(Graphics);
+import credentials from './credentials.json';
 
 function start(){
   g.setFont8x12();
@@ -9,3 +10,15 @@ function start(){
 let s = new SPI();
 s.setup({mosi: 13, sck: 14});
 let g = require("SSD1306").connectSPI(s, 12 /* DC */, 5 /* RST */, start);
+
+let wifi = require("Wifi");
+
+function onConnect(err) {
+    if(err) {
+        console.log("An error has occured :( ", err.message);
+    } else {
+        console.log("Connected with IP : ", wifi.getIP().ip);
+    }
+}
+
+wifi.connect(credentials['ssid'], { password: credentials['password'] }, onConnect);
