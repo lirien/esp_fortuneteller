34d252a1eaf9c2d4c2e983fa4a9141157a18bd34
Erin Sullivan <kittylily@gmail.com>
2016-10-26 17:15:54 -0700

    ✨ Code cleanup

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-require("Font8x12").add(Graphics);
-import credentials from './credentials.json';
+require('Font8x12').add(Graphics);
+const wifi = require('Wifi');
+const ssd1306 = require('SSD1306');
+import credentials from './credentials.js';
 
-function start(){
+function start() {
   g.setFont8x12();
-  g.drawString("ESP8266 Crystal Ball",0,0);
+  g.drawString('ESP8266 Crystal Ball',0,0);
   g.flip();
 }
 
 let s = new SPI();
 s.setup({mosi: 13, sck: 14});
-let g = require("SSD1306").connectSPI(s, 12 /* DC */, 5 /* RST */, start);
-
-let wifi = require("Wifi");
+const g = ssd1306.connectSPI(s, 12 /* DC */, 5 /* RST */, start);
 
 function onConnect(err) {
-    if(err) {
-        console.log("An error has occured :( ", err.message);
-    } else {
-        console.log("Connected with IP : ", wifi.getIP().ip);
-    }
+  if(err) {
+    console.log("An error has occured :( ", err.message);
+  }
+  else {
+    console.log("Connected with IP : ", wifi.getIP().ip);
+  }
 }
 
 wifi.connect(credentials['ssid'], { password: credentials['password'] }, onConnect);
